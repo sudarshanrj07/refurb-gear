69bb669faef19e12097fd6fa92d7e075ee23c69c
sudarshanrj07 <srjalawadi07@gmail.com>
2025-07-18 14:16:20 +0530

    created user monogoose model

--- api/index.js
+++ api/index.js
@@ -1,16 +1,27 @@
 import express from "express";
 import "dotenv/config";
 import { DBConnect } from "./utils/connection.js";
+import routes from "./routes/routes.js";
 
+//dotenv
 const { PORT, DB_URL } = process.env;
 
 const app = express();
-DBConnect(DB_URL);
+
+//DB connection
+DBConnect(DB_URL).catch(() => {
+	console.log("DB connection request failed");
+});
+
+//gloabla middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//routes
+app.use("/api", routes);
 
 app.get("/", (req, res) => {
 	res.send("Hello from a diferent port");
 });
 
-app.listen(PORT, () => {
-	console.log(`Connected to PORT: ${PORT}`);
-});
+app.listen(PORT);
